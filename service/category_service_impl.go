084acdddb460dcb08c5bd91518d9a8d5ff44dd7b
dedybastiant <dedybastian.t@gmail.com>
2023-10-31 15:36:51 +0700

    Add validator

--- service/category_service_impl.go
+++ service/category_service_impl.go
@@ -7,14 +7,20 @@ import (
 	"belajar-golang-restful-api/repository"
 	"context"
 	"database/sql"
+
+	"github.com/go-playground/validator/v10"
 )
 
 type CategoryServiceImpl struct {
 	CategoryRepository repository.CategoryRepository
 	DB                 *sql.DB
+	Validate           *validator.Validate
 }
 
 func (service *CategoryServiceImpl) Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse {
+	err := service.Validate.Struct(request)
+	helper.PanicIfErr(err)
+
 	tx, err := service.DB.Begin()
 	helper.PanicIfErr(err)
 	defer helper.RollbackOrCommit(tx)
@@ -28,6 +34,9 @@ func (service *CategoryServiceImpl) Create(ctx context.Context, request web.Cate
 }
 
 func (service *CategoryServiceImpl) Udpate(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse {
+	err := service.Validate.Struct(request)
+	helper.PanicIfErr(err)
+
 	tx, err := service.DB.Begin()
 	helper.PanicIfErr(err)
 	defer helper.RollbackOrCommit(tx)
